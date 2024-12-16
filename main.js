d0a47ac7430e154adbbaaa634c506fae3634890b
Nathaniel Johnston <48165052+nathanielwjohnston@users.noreply.github.com>
2024-12-16 22:56:23 +0000

    Show functions work

--- main.js
+++ main.js
@@ -9,6 +9,29 @@ list.append("hamster");
 list.append("snake");
 list.append("turtle");
 
-console.log(list.insertAt("test", 0));
+list.prepend("prepend");
+
+console.log(list.size()); // 7
+
+console.log(list.getHead()); // prepend node
+
+console.log(list.tail()); // turtle node
+
+console.log(list.at(2)); // cat node
+
+list.pop();
+
+console.log(list.contains("turtle")); // false
+
+console.log(list.find("hamster")); // 4
 
 console.log(list.toString());
+// ( prepend ) -> ( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> null
+
+list.insertAt("test", 3);
+console.log(list.toString());
+// ( prepend ) -> ( dog ) -> ( cat ) -> ( test ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> null
+
+list.removeAt(1);
+console.log(list.toString());
+// ( prepend ) -> ( cat ) -> ( test ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> null
